53202356f32daa3050d67619d8709f04ed0822c9
proxyman69420 <georgevonmailington@gmail.com>
2025-09-16 16:58:47 -0500

    even more stuff

--- assets/js/freepackdata.js
+++ assets/js/freepackdata.js
@@ -2,18 +2,18 @@ const freePackData = [
 {
     name: "Farm Animal Pack",
     id: "farm_animal_pack",
-    bg: "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+    bg: "radial-gradient(circle, #0bc2cf 0%, #349aef 100%)",
     blook: ["Chick", "Chicken", "Cow", "Goat", "Horse", "Pig", "Sheep", "Duck", "Alpaca"],
     chance: [11.111111, 11.111111, 11.111111, 11.111111, 11.111111, 11.111111, 11.111111, 11.111111, 11.111111],
     svg: ["https://ac.blooket.com/marketassets/blooks/chick.svg", "https://ac.blooket.com/marketassets/blooks/chicken.svg", "https://ac.blooket.com/marketassets/blooks/cow.svg", "https://ac.blooket.com/marketassets/blooks/goat.svg", "https://ac.blooket.com/marketassets/blooks/horse.svg", "https://ac.blooket.com/marketassets/blooks/pig.svg", "https://ac.blooket.com/marketassets/blooks/sheep.svg", "https://ac.blooket.com/marketassets/blooks/duck.svg", "https://ac.blooket.com/marketassets/blooks/alpaca.svg"],
-    png: "",
+    png: "./assets/packs/farm-full.png",
     rarity: ["Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common"],
 
     colorBlook: [],
     colorSvg: [],
     colorRarity: [],
 
-    packBottom: "",
+    packBottom: "./assets/packs/farm-bottom.png",
     cornerIcon: "https://ac.blooket.com/dashclassic/assets/Nav_Icon_Farm-DLJOEU1H.svg",
     blookBackground: "https://ac.blooket.com/dashclassic/assets/Highlighted_Background_Farm-Cf8P0mJz.svg",
 
@@ -25,18 +25,18 @@ const freePackData = [
 {
     name: "Pet Pack",
     id: "pet_pack",
-    bg: "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+    bg: "radial-gradient(circle, #0bc2cf 0%, #349aef 100%)",
     blook: ["Dog", "Cat", "Rabbit", "Goldfish", "Hamster", "Turtle", "Kitten", "Puppy"],
     chance: [12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5],
     svg: ["https://ac.blooket.com/marketassets/blooks/dog.svg", "https://ac.blooket.com/marketassets/blooks/cat.svg", "https://ac.blooket.com/marketassets/blooks/rabbit.svg", "https://ac.blooket.com/marketassets/blooks/goldfish.svg", "https://ac.blooket.com/marketassets/blooks/hamster.svg", "https://ac.blooket.com/marketassets/blooks/turtle.svg", "https://ac.blooket.com/marketassets/blooks/kitten.svg", "https://ac.blooket.com/marketassets/blooks/puppy.svg"],
-    png: "",
+    png: "./assets/packs/pet-full.png",
     rarity: ["Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common"],
 
     colorBlook: [],
     colorSvg: [],
     colorRarity: [],
 
-    packBottom: "",
+    packBottom: "./assets/packs/pet-bottom.png",
     cornerIcon: "https://ac.blooket.com/dashclassic/assets/Nav_Icon_Pets-BgCBMaDh.svg",
     blookBackground: "https://ac.blooket.com/dashclassic/assets/Highlighted_Background_Pets--9tpvwHe.svg",
 
@@ -48,18 +48,18 @@ const freePackData = [
 {
     name: "Forest Animal Pack",
     id: "forest_animal_pack",
-    bg: "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+    bg: "radial-gradient(circle, #0bc2cf 0%, #349aef 100%)",
     blook: ["Bear", "Moose", "Fox", "Raccoon", "Squirrel", "Owl", "Hedgehog", "Deer", "Wolf", "Beaver"],
     chance: [10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0],
     svg: ["https://ac.blooket.com/marketassets/blooks/bear.svg", "https://ac.blooket.com/marketassets/blooks/moose.svg", "https://ac.blooket.com/marketassets/blooks/fox.svg", "https://ac.blooket.com/marketassets/blooks/raccoon.svg", "https://ac.blooket.com/marketassets/blooks/squirrel.svg", "https://ac.blooket.com/marketassets/blooks/owl.svg", "https://ac.blooket.com/marketassets/blooks/hedgehog.svg", "https://ac.blooket.com/marketassets/blooks/deer.svg", "https://ac.blooket.com/marketassets/blooks/wolf.svg", "https://ac.blooket.com/marketassets/blooks/beaver.svg"],
-    png: "",
+    png: "./assets/packs/forest-full.png",
     rarity: ["Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common"],
 
     colorBlook: [],
     colorSvg: [],
     colorRarity: [],
 
-    packBottom: "",
+    packBottom: "./assets/packs/forest-bottom.png",
     cornerIcon: "https://ac.blooket.com/dashclassic/assets/Nav_Icon_Forest-D7iXnxVN.svg",
     blookBackground: "https://ac.blooket.com/dashclassic/assets/Highlighted_Background_Forest-56I9sRnu.svg",
 
@@ -71,18 +71,18 @@ const freePackData = [
 {
     name: "Tropical Animal Pack",
     id: "tropical_animal_pack",
-    bg: "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+    bg: "radial-gradient(circle, #0bc2cf 0%, #349aef 100%)",
     blook: ["Tiger", "Orangutan", "Cockatoo", "Parrot", "Anaconda", "Jaguar", "Macaw", "Toucan", "Panther", "Capuchin", "Gorilla", "Hippo", "Rhino", "Giraffe"],
     chance: [7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857, 7.142857],
     svg: ["https://ac.blooket.com/marketassets/blooks/tiger.svg", "https://ac.blooket.com/marketassets/blooks/orangutan.svg", "https://ac.blooket.com/marketassets/blooks/cockatoo.svg", "https://ac.blooket.com/marketassets/blooks/parrot.svg", "https://ac.blooket.com/marketassets/blooks/anaconda.svg", "https://ac.blooket.com/marketassets/blooks/jaguar.svg", "https://ac.blooket.com/marketassets/blooks/macaw.svg", "https://ac.blooket.com/marketassets/blooks/toucan.svg", "https://ac.blooket.com/marketassets/blooks/panther.svg", "https://ac.blooket.com/marketassets/blooks/capuchinmonkey.svg", "https://ac.blooket.com/marketassets/blooks/gorilla.svg", "https://ac.blooket.com/marketassets/blooks/hippo.svg", "https://ac.blooket.com/marketassets/blooks/rhino.svg", "https://ac.blooket.com/marketassets/blooks/giraffe.svg"],
-    png: "",
+    png: "./assets/packs/tropical-full.png",
     rarity: ["Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common"],
 
     colorBlook: [],
     colorSvg: [],
     colorRarity: [],
 
-    packBottom: "",
+    packBottom: "./assets/packs/tropical-bottom.png",
     cornerIcon: "https://ac.blooket.com/dashclassic/assets/Nav_Icon_Tropical-DFEdkGBU.svg",
     blookBackground: "https://ac.blooket.com/dashclassic/assets/Highlighted_Background_Tropical-DV9rwhsh.svg",
 
@@ -94,18 +94,18 @@ const freePackData = [
 {
     name: "Arctic Animal Pack",
     id: "arctic_animal_pack",
-    bg: "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+    bg: "radial-gradient(circle, #0bc2cf 0%, #349aef 100%)",
     blook: ["Snowy Owl", "Polar Bear", "Arctic Fox", "Baby Penguin", "Penguin", "Arctic Hare", "Seal", "Walrus"],
     chance: [12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5],
     svg: ["https://ac.blooket.com/marketassets/blooks/snowyowl.svg", "https://ac.blooket.com/marketassets/blooks/polarbear.svg", "https://ac.blooket.com/marketassets/blooks/arcticfox.svg", "https://ac.blooket.com/marketassets/blooks/babypenguin.svg", "https://ac.blooket.com/marketassets/blooks/penguin.svg", "https://ac.blooket.com/marketassets/blooks/arctichare.svg", "https://ac.blooket.com/marketassets/blooks/seal.svg", "https://ac.blooket.com/marketassets/blooks/walrus.svg"],
-    png: "",
+    png: "./assets/packs/arctic-full.png",
     rarity: ["Common", "Common", "Common", "Common", "Common", "Common", "Common", "Common"],
 
     colorBlook: [],
     colorSvg: [],
     colorRarity: [],
 
-    packBottom: "",
+    packBottom: "./assets/packs/arctic-bottom.png",
     cornerIcon: "https://ac.blooket.com/dashclassic/assets/Nav_Icon_Arctic-DzaIX0Uv.svg",
     blookBackground: "https://ac.blooket.com/dashclassic/assets/Highlighted_Background_Arctic-DnFet-Es.svg",
 
